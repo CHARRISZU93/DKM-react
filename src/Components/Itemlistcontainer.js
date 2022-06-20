38d9314484a6cc1af47ad1d640cfbc6f6a46242d
Manuel Charris <charriszu930928@gmail.com>
2022-06-20 17:06:51 -0500

    LAND ING

---
+++ src/Components/Itemlistcontainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../Components/body.css'
+
+const Itemlistcntr = () => {
+    return (
+        <h1>A DIFFERENT KIND OF MARKETING</h1>
+    )
+}
+
+export default Itemlistcntr
