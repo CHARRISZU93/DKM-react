6ddaae33091666c93954022a819225b2c81a1ad0
Manuel Charris <charriszu930928@gmail.com>
2022-08-07 23:03:37 -0500

    final1

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -2,12 +2,13 @@ import React, { useContext, useEffect, useState } from "react";
 import { contexto } from '../Context/CartContext'
 import { db } from './Firebase'
 import { doc, addDoc, collection, serverTimestamp, updateDoc } from "firebase/firestore"
+import { TextField } from "@mui/material";
 
 
 const Cart = () => {
 
     const { Service, addProduct, deleteProduct, clear, getQtyServicios } = useContext(contexto)
-    const { idVenta, SetIdVenta } = useState("")
+    const [idVenta, SetIdVenta] = useState("")
     const [DatosComprador, SetDatosComprador] = useState({
         nombre: "",
         apellido: "",
@@ -43,53 +44,59 @@ const Cart = () => {
                 <h1>Datos del Comprador</h1>,
                 <form>
                     <label>Nombre:</label>,
-                    <input id="nombre" onChange={Cambio.bind(this)} type="text" name="nombre" value={DatosComprador.nombre} />,
+                    <TextField id="nombre" onChange={Cambio} type="text" name="nombre" value={DatosComprador.nombre} />,
                     <label>Apellido:</label>,
-                    <input id="apellido" onChange={Cambio} type="text" name="apellido" />,
+                    <TextField id="apellido" onChange={Cambio} type="text" name="apellido" value={DatosComprador.apellido} />,
                     <label>Direccion:</label>,
-                    <input id="direccion" onChange={Cambio} type="text" name="direccion" />,
+                    <TextField id="direccion" onChange={Cambio} type="text" name="direccion" value={DatosComprador.direccion} />,
                     <label>Telefono:</label>,
-                    <input id="telefono" onChange={Cambio} type="text" name="telefono" />,
+                    <TextField id="telefono" onChange={Cambio} type="text" name="telefono" value={DatosComprador.telefono} />,
                     <label>Email:</label>,
-                    <input id="email" onChange={Cambio} type="text" name="email" />,
+                    <TextField id="email" onChange={Cambio} type="text" name="email" value={DatosComprador.email} />,
                 </form>
             </div>
         )
     }
 
-    const FinalizarCompra = () => {
+    const FinalizarCompra = async () => {
         const VentasCollection = collection(db, 'VENTAS')
 
-        addDoc(VentasCollection, {
-            DatosComprador,
-            Items: ({ addProduct }),
+        await addDoc(VentasCollection, {
+            Comprador: DatosComprador,
+            Items: Service,
             Fecha: serverTimestamp(),
-            Total: 500
         })
             .then((result) => {
+                console.log(result)
                 SetIdVenta(result.id)
+                clear()
             });
-        const UpdateCollection = doc(db, 'SERVICIOS')
-        updateDoc(UpdateCollection, { stock1: 10 })
     }
 
     return (
         <>
-            {!Service.id
+            {!Service.id && !idVenta
                 ? <h1>No hay productos en el carrito</h1>
-                : <>
-                    <div className="campo">
-                        <h1 className="campologoservicio">{Service.logo}</h1>
-                        <p className="campodescrp">Plan {Service.name}</p>
-                        <p className="campodescrp">Precio Desde USD {Service.price}</p>
-                    </div>
-                    <button onClick={() => clear()}>Vaciar carrito</button>
-                    <button onClick={() => deleteProduct(Service.id)}>Eliminar producto</button>
-                    <Comprador />
-                    <button onClick={() => FinalizarCompra()}>Finalizar compra</button>
-
-                </>
-
+                : (!idVenta
+                    ?
+                    <>
+                        <div className="campo">
+                            <h1 className="campologoservicio">{Service.logo}</h1>
+                            <p className="campodescrp">Plan {Service.name}</p>
+                            <p className="campodescrp">Precio Desde USD {Service.price}</p>
+                        </div>
+                        <button onClick={() => clear()}>Vaciar carrito</button>
+                        <button onClick={() => deleteProduct(Service.id)}>Eliminar producto</button>
+                        {Comprador()}
+                        <h1>Total: {getQtyServicios()}</h1>
+                        <button onClick={() => FinalizarCompra()}>Finalizar compra</button>
+                    </>
+                    :
+                    <>
+                        <h1>Compra finalizada</h1>
+                        <p>Su compra se ha realizado con exito, su numero de venta es: {idVenta}</p>
+                    </>
+                )
             }
         </>
     )
